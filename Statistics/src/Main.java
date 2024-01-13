31fe2c356a9d9627a915102be8ee2e28d10c4f7c
Dexter <dexter.t.jones@outlook.com>
2024-01-12 19:39:43 -0700

    Added pseudocode for source code

---
+++ Statistics/src/Main.java
@@ -0,0 +1,24 @@
+/*
+Written by Dexter Jones
+
+Pseudocode:
+A program that provides important statistics for grades in a class.
+
+for-loop:
+    Read 10 floating-point grades from user input
+
+input: float grades
+output: float average, maximum, minimum
+
+user validation: while loop try-catch
+    try: parse input for float
+    catch: number formatting exception - ask for user input again
+ */
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+}

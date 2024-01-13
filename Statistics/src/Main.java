788a2d2a9afd19cca0be0b4ab357a3a72f3cb118
Dexter <dexter.t.jones@outlook.com>
2024-01-12 20:44:36 -0700

    Added for loops for user input and while loop for user validation. Having issue filling elements of the grades array with each grade, so far, it is adding only the first input into each element, may be from grade parsing from s but not updating before grades array pulling variable data.

--- Statistics/src/Main.java
+++ Statistics/src/Main.java
@@ -14,11 +14,37 @@ user validation: while loop try-catch
     try: parse input for float
     catch: number formatting exception - ask for user input again
  */
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        float ave, max, min, grade = 0;
+        String s;
+        boolean isNumber = false;
+        float grades[] = new float[10]; // float array to hold all grades
 
+        System.out.print("Academic Performance Analyzer\n--------------------------------------\n ");
 
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Please enter grade " + (i+1) + ":");
+            s = scnr.nextLine();
+
+            while (!isNumber) {
+                try { // try to parse user input s to float
+                    grade = Float.parseFloat(s);
+                    isNumber = true;
+                }
+                catch (NumberFormatException e){ // if unable to parse String s to float, ask for user input again
+                        System.out.print("Invalid entry. Please enter a number for the grade: ");
+                        s = scnr.nextLine();
+                }
+            }
+            grades[i] = grade;
+        }
+        for (int i = 0; i < 10; i++) { // debugging for loop - delete
+            System.out.println(grades[i]);
+        }
     }
 }

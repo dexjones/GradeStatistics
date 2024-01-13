47bce24b81b2a16dbc4dfe6aaa0f950e13ebc256
Dexter <dexter.t.jones@outlook.com>
2024-01-13 14:19:11 -0700

    Completed Version

--- Statistics/src/Main.java
+++ Statistics/src/Main.java
@@ -19,15 +19,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner scnr = new Scanner(System.in);
-        float ave, max = 0, min = 101, grade = 0, total = 0;
+        float ave = 0, max = 0, min = 101, grade = 0, total = 0;
+        int size = 10;
         String s;
         boolean isNumber = false;
-        float grades[] = new float[10]; // float array to hold all grades
 
-        System.out.print("Academic Performance Analyzer\n--------------------------------------\n ");
+        System.out.print("Academic Performance Analyzer\n--------------------------------------\n");
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < size; i++) {
             isNumber = false; // needed to reset while entry variable
             System.out.print("Please enter grade " + (i+1) + ":");
             s = scnr.nextLine();
@@ -36,7 +37,7 @@ public class Main {
                 try { // try to parse user input s to float
                     grade = Float.parseFloat(s);
 
-                    while (grade < 0 || grade > 100) {
+                    while (grade < 0 || grade > 100) { // boundary check for grade -- no extra credit allowed
                         System.out.print("Number is out of bounds. Please enter a grade between 0-100: ");
                         s = scnr.nextLine();
                         try {
@@ -60,13 +61,9 @@ public class Main {
                         s = scnr.nextLine();
                 }
             }
-            grades[i] = grade;
-
         }
-        System.out.println(total);
-
-        for (int i = 0; i < 10; i++) { // debugging for loop - delete
-            System.out.println(grades[i]);
-        }
+        ave = total / size;
+        System.out.println("\nThe grade statistics of the class:\n");
+        System.out.print("Average: " + ave + "\nMaximum: " + max + "\nMinimum: " + min);
     }
 }
